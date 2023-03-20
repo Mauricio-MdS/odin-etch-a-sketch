a48c5c4b462bb143e2dc279cd5be5db6688cee71
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-03-20 20:46:36 -0300

    feat: add button for a new grid

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
+const button = document.querySelector("button");
 const container = document.querySelector(".container");
 
-// Create a grid of 16X16 square divs.
-for (i = 1; i <= 256; i++) {
-    const div = document.createElement("div");
-    div.classList.add("square");
-    div.addEventListener("mouseover", (event) => paintSquare(event.target));
-    container.appendChild(div);
+
+newGrid(16);
+
+button.addEventListener("click", askGrid );
+
+function askGrid() {
+    let n;
+    do {
+        n = prompt("Choose N for a N X N grid. It must be less than 100.");
+        n = isNaN(n) ? 0 : Math.floor(n);
+    } while (n < 1 || n > 100)
+    newGrid(n);
+}
+
+// Create a grid of nXn square divs.
+function newGrid(n) {
+
+    container.innerHTML = "";
+
+    const height = 100/n;
+    for (i = 1; i <= (n * n); i++) {
+        const div = document.createElement("div");
+        div.classList.add("square");
+        div.style.cssText = `height: ${height}%; aspect-ratio: 1;`;
+        div.addEventListener("mouseover", (event) => paintSquare(event.target));
+        container.appendChild(div);
+    }
+
 }
 
 function paintSquare(square) {
